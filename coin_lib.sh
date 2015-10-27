c768e8456b8f49071c1037dd1e235d7d6a6140b4
Boris Rybalkin <ribalkin@gmail.com>
2015-10-27 08:41:18 +0000

    adding pip for chroot build

--- coin_lib.sh
+++ coin_lib.sh
@@ -8,6 +8,10 @@ if [ ! -z "$TEAMCITY_VERSION" ]; then
   rm -rf /tmp/coin.cache
 fi
 
+wget --no-check-certificate --progress=dot:giga -O /tmp/get-pip.py https://bootstrap.pypa.io/get-pip.py 2>&1
+python /tmp/get-pip.py
+pip install coin
+
 if [ ! -d lib ]; then
   mkdir lib
 fi
@@ -15,7 +19,7 @@ fi
 rm -rf lib/*
 
 cd lib
-pip install coin
+
 coin py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
 coin py https://pypi.python.org/packages/source/s/syncloud-app/syncloud-app-0.38.tar.gz
 cd ..
