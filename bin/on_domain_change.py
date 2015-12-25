9f9324f90160cee68410cd282843f9b94cff5d21
Boris Rybalkin <ribalkin@gmail.com>
2015-12-25 22:47:58 +0000

    disabling domain change event handler as assets re-compilation is too slow

--- bin/on_domain_change.py
+++ bin/on_domain_change.py
@@ -1,11 +1,13 @@
-from os.path import dirname, join, abspath, isdir
-from os import listdir
-import sys
-
-app_path = abspath(join(dirname(__file__), '..', '..', 'diaspora'))
-lib_path = join(app_path, 'lib')
-libs = [join(lib_path, item) for item in listdir(lib_path) if isdir(join(lib_path, item))]
-map(sys.path.append, libs)
-
-from diaspora.installer import DiasporaInstaller
-DiasporaInstaller().update_domain()
+# from os.path import dirname, join, abspath, isdir
+# from os import listdir
+# import sys
+#
+# app_path = abspath(join(dirname(__file__), '..', '..', 'diaspora'))
+# lib_path = join(app_path, 'lib')
+# libs = [join(lib_path, item) for item in listdir(lib_path) if isdir(join(lib_path, item))]
+# map(sys.path.append, libs)
+#
+# from diaspora.installer import DiasporaInstaller
+#
+# Regenerate recompilation is too slow to react on domain change
+# DiasporaInstaller().update_domain()
