dfa7038ffc1d608080473e84e61337c90e65e93a
Vladimir Sapronov <vladimir.sapronov@gmail.com>
2015-12-28 00:57:04 -0500

    Moving to syncloud-lib

--- coin_lib.sh
+++ coin_lib.sh
@@ -17,6 +17,6 @@ rm -rf lib/*
 cd lib
 
 coin py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
-coin py https://pypi.python.org/packages/source/s/syncloud-app/syncloud-app-0.38.tar.gz
+coin py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz
 coin py http://build.syncloud.org:8111/guestAuth/repository/download/thirdparty_PyYAML_x86_64/lastSuccessful/PyYAML-x86_64.tar.gz
 cd ..
