8aa3a39ec717305fe5d6568dcb877c6a6d423d10
Boris Rybalkin <ribalkin@gmail.com>
2016-04-06 21:44:37 +0100

    upgrading to /rest/activate

--- integration/verify.py
+++ integration/verify.py
@@ -86,7 +86,7 @@ def test_activate_device(auth):
     run_ssh('/opt/app/sam/bin/sam update --release {0}'.format(release), password=DEFAULT_DEVICE_PASSWORD)
     run_ssh('/opt/app/sam/bin/sam --debug upgrade platform', password=DEFAULT_DEVICE_PASSWORD)
 
-    response = requests.post('http://localhost:81/server/rest/activate',
+    response = requests.post('http://localhost:81/rest/activate',
                              data={'main_domain': 'syncloud.info', 'redirect_email': email, 'redirect_password': password,
                                    'user_domain': domain, 'device_username': DEVICE_USER, 'device_password': DEVICE_PASSWORD})
     assert response.status_code == 200
