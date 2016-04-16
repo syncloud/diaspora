9a3f2fbea89d6c248dec38f8b3b870b60a9fad3b
Boris Rybalkin <ribalkin@gmail.com>
2016-04-16 11:24:28 +0100

    using relative coin cache

--- coin_lib.sh
+++ coin_lib.sh
@@ -3,21 +3,12 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
-if [ ! -z "$TEAMCITY_VERSION" ]; then
-  echo "running under TeamCity, cleaning coin cache"
-  rm -rf /tmp/coin.cache
-fi
-
 if [ ! -d lib ]; then
   mkdir lib
 fi
 
 rm -rf lib/*
 
-cd lib
-
-coin py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
-coin py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz
-coin py http://build.syncloud.org:8111/guestAuth/repository/download/thirdparty_PyYAML_x86_64/lastSuccessful/PyYAML-x86_64.tar.gz
-
-cd ..
+coin --to lib py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
+coin --to lib py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz
+coin --to lib py http://build.syncloud.org:8111/guestAuth/repository/download/thirdparty_PyYAML_x86_64/lastSuccessful/PyYAML-x86_64.tar.gz
