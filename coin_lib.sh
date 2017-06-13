bad2de3bb5d4654c9f7409154c1555231e95a5c8
Boris Rybalkin <ribalkin@gmail.com>
2017-06-13 21:21:09 +0100

    fix 3rdparty

--- coin_lib.sh
+++ coin_lib.sh
@@ -11,4 +11,4 @@ rm -rf lib/*
 
 coin --to lib py https://pypi.python.org/packages/2.7/r/requests/requests-2.7.0-py2.py3-none-any.whl
 coin --to lib py https://pypi.python.org/packages/source/s/syncloud-lib/syncloud-lib-2.tar.gz
-coin --to lib py http://build.syncloud.org:8111/guestAuth/repository/download/thirdparty_PyYAML_x86_64/lastSuccessful/PyYAML-x86_64.tar.gz
+coin --to lib py http://artifact.syncloud.org/3rdparty/PyYAML-x86_64.tar.gz
