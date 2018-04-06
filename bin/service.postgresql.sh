aa566d4a1bd19e669d7f6187ff6aab466bf29a4d
Boris Rybalkin <ribalkin@gmail.com>
2018-04-06 14:41:02 +0100

    nginx service

---
+++ bin/service.postgresql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
+
+if [[ -z "$1" ]]; then
+    echo "usage $0 [start]"
+    exit 1
+fi
+
+case $1 in
+start)
+    exec ${DIR}/postgresql/bin/pg_ctl -w -s -D ${SNAP_COMMON}/database start
+    ;;
+stop)
+    exec ${DIR}/postgresql/bin/pg_ctl -s -D ${SNAP_COMMON}/database stop -m fast
+    ;;
+*)
+    echo "not valid command"
+    exit 1
+    ;;
+esac
