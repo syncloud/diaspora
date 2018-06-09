3bd77857bdf925f29c45addbbc8914263827003f
Boris Rybalkin <ribalkin@gmail.com>
2018-06-09 14:28:06 +0100

    fix desktop profile

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -45,7 +45,7 @@ def new_driver(profile):
 
 @pytest.fixture(scope="module")
 def driver():
-    profile = new_profile("Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16")
+    profile = new_profile("Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0")
     driver = new_driver(profile)
     driver.set_window_position(0, 0)
     driver.set_window_size(1280, 2000)
@@ -54,7 +54,7 @@ def driver():
     
 @pytest.fixture(scope="module")
 def mobile_driver():    
-    profile =  new_profile("Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0")
+    profile = new_profile("Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16")
     driver = new_driver(profile)
     driver.set_window_position(0, 0)
     driver.set_window_size(400, 2000)
