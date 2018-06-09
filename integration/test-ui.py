69d5c2d972c589867b461fbb943e9310f7b52977
Boris Rybalkin <ribalkin@gmail.com>
2018-06-09 11:02:48 +0100

    switch default user agent back to desktop for now

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -24,7 +24,7 @@ def profile():
     profile.set_preference('app.update.auto', False)
     profile.set_preference('app.update.enabled', False)
     mobile_user_agent = "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"
-    profile.set_preference("general.useragent.override", mobile_user_agent)
+    #profile.set_preference("general.useragent.override", mobile_user_agent)
     return profile
 
 
