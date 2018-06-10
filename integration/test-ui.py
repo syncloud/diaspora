ef1ed2d95cc008e85f02e51e663811bbc6818b39
Boris Rybalkin <ribalkin@gmail.com>
2018-06-10 14:44:56 +0100

    fix js log

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -160,11 +160,11 @@ def screenshots(driver, name):
  
     driver.get_screenshot_as_file(join(screenshot_dir, '{0}.png'.format(name)))
   
-    with open(join(screenshot_dir, '{0}.html'.format(name)), "w") as f:
+    with open(join(screenshot_dir, '{0}.html.log'.format(name)), "w") as f:
         f.write(driver.page_source.encode("utf-8"))
    
     with open(join(screenshot_dir, '{0}.js.log'.format(name)), "w") as f:
-        f.write(driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []'))
+        f.write(str(driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')))
 
 
  
