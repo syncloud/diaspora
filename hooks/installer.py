4421d765cf2adb1f021c15b8ce58d296d81619fa
Boris Rybalkin <ribalkin@gmail.com>
2018-06-14 21:45:28 +0100

    create tmp on storage

--- hooks/installer.py
+++ hooks/installer.py
@@ -132,7 +132,7 @@ class DiasporaInstaller:
         if 'SNAP' not in environ:
             self.log.info(fs.chownpath(self.app_dir, USER_NAME, recursive=True))
 
-        self.update_configuraiton()
+        self.update_configuration()
 
         self.log.info("setup systemd")
 
@@ -165,7 +165,7 @@ class DiasporaInstaller:
         app.add_service(SYSTEMD_NGINX_NAME)
 
     def configure(self):
-        storage.init_storage(APP_NAME, USER_NAME)
+        self.prepare_storage()
         UserConfig(self.app_data_dir).set_activated(True)
 
     def remove(self):
@@ -181,9 +181,9 @@ class DiasporaInstaller:
             shutil.rmtree(self.app_dir)
         
     def update_domain(self):
-        self.update_configuraiton()
+        self.update_configuration()
 
-    def update_configuraiton(self):
+    def update_configuration(self):
 
         diaspora_config = yaml.load(open(self.diaspora_config))
 
@@ -191,3 +191,13 @@ class DiasporaInstaller:
         diaspora_config['configuration']['environment']['assets']['host'] = self.app_url
 
         yaml.dump(diaspora_config, open(self.diaspora_config, 'w'))
+
+    def on_storage_change(self):
+        self.prepare_storage()
+
+    def prepare_storage(self):
+        storage.init_storage(APP_NAME, USER_NAME)
+        storage_dir = storage.get_storage_dir(APP_NAME)
+        tmp_dir = join(storage_dir, 'tmp')
+        fs.makepath(tmp_dir)
+        fs.chownpath(tmp_dir, USER_NAME)
---
+++ hooks/storage-change.py
@@ -0,0 +1,3 @@
+from installer import DiasporaInstaller
+installer = DiasporaInstaller()
+installer.on_storage_change()
