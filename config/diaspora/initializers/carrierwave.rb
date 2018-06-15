c074e5bec138eeae8f0ea883c5838f1c7b60b739
Boris Rybalkin <ribalkin@gmail.com>
2018-06-15 18:07:11 +0100

    temove CarrierWave config fixes for now

--- config/diaspora/initializers/carrierwave.rb
+++ config/diaspora/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
-    config.root = '/data/diaspora'
-    config.store_dir = '/data/diaspora/uploads'
-    config.cache_dir = '/data/diaspora/cache'
+    #config.root = '/data/diaspora'
+    #config.store_dir = '/data/diaspora/uploads'
+    #config.cache_dir = '/data/diaspora/cache'
     config.storage = :file
 end
