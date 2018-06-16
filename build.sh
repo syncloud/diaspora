772be6cc580bb31ecc1f267dc9e075a8f73d50a9
Boris Rybalkin <ribalkin@gmail.com>
2018-06-16 08:59:49 +0100

    exclude specs

--- build.sh
+++ build.sh
@@ -153,9 +153,9 @@ ldd ${BUILD_DIR}/ruby/lib/libpq.so
 ${BUILD_DIR}/diaspora/bin/rake assets:precompile
 
 set +e
-grep -r "Rails.root" .
-grep -r "uploads" .
-grep -r "tmp" .
+grep -r "Rails.root" . | grep -v "_spec.rb"
+grep -r "uploads" . | grep -v "_spec.rb"
+grep -r "tmp" . | grep -v "_spec.rb"
 set -e
 
 rm config/diaspora.yml
