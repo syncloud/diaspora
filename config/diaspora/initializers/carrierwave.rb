40ac0394f02d0e4851c9160de6ba0ba1baa64692
Boris Rybalkin <ribalkin@gmail.com>
2018-06-18 18:18:12 +0100

    clean not used patches and tweaks

--- config/diaspora/initializers/carrierwave.rb
+++
@@ -1,6 +0,0 @@
-CarrierWave.configure do |config|
-    #config.root = '/data/diaspora'
-    #config.store_dir = '/data/diaspora/uploads'
-    #config.cache_dir = '/data/diaspora/cache'
-    config.storage = :file
-end
