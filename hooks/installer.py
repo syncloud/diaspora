09cdbcca7c93e68cc3f10ea69dae78089bab8376
Boris Rybalkin <ribalkin@gmail.com>
2018-09-23 20:44:21 +0100

    remove lib sys path

--- hooks/installer.py
+++ hooks/installer.py
@@ -2,12 +2,6 @@ from os.path import dirname, join, abspath, isdir
 from os import listdir
 import sys
 
-app_path = abspath(join(dirname(__file__), '..'))
-
-lib_path = join(app_path, 'lib')
-libs = [join(lib_path, item) for item in listdir(lib_path) if isdir(join(lib_path, item))]
-map(lambda l: sys.path.insert(0, l), libs)
-
 from os import environ, symlink
 from os.path import isdir, join
 import shutil
