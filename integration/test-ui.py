5e4833ead714bf9671fa43ebfc47f81355db0ee7
Boris Rybalkin <ribalkin@gnail.com>
2020-04-26 20:01:17 +0100

    fix ui tests

--- integration/test-ui.py
+++ integration/test-ui.py
@@ -18,65 +18,39 @@ log_dir = join(LOG_DIR, 'app_log')
 screenshot_dir = join(DIR, 'screenshot')
 
 
-def new_profile(user_agent):
-    profile = webdriver.FirefoxProfile()
-    profile.add_extension('/tools/firefox/JSErrorCollector.xpi')
-    profile.set_preference('app.update.auto', False)
-    profile.set_preference('app.update.enabled', False)
-    profile.set_preference("general.useragent.override", user_agent)
+@pytest.fixture(scope="session")
+def module_setup(request, device, log_dir, ui_mode, artifact_dir):
+    def module_teardown():
+        tmp_dir = '/tmp/syncloud/ui'
+        device.activated()
+        device.run_ssh('mkdir -p {0}'.format(tmp_dir), throw=False)
+        device.run_ssh('journalctl > {0}/journalctl.ui.{1}.log'.format(tmp_dir, ui_mode), throw=False)
+        device.run_ssh('cp /var/log/syslog {0}/syslog.ui.{1}.log'.format(tmp_dir, ui_mode), throw=False)
+      
+        device.scp_from_device('{0}/*'.format(tmp_dir), artifact_dir)
+        check_output('chmod -R a+r {0}'.format(artifact_dir), shell=True)
 
-    return profile
-
-def new_driver(profile):
-
-    if exists(screenshot_dir):
-        shutil.rmtree(screenshot_dir)
-    os.mkdir(screenshot_dir)
-
-    firefox_path = '/tools/firefox/firefox'
-    caps = DesiredCapabilities.FIREFOX
-    caps["marionette"] = True
-    caps['acceptSslCerts'] = True
-
-    binary = FirefoxBinary(firefox_path)
-
-    return webdriver.Firefox(profile, capabilities=caps, log_path="{0}/firefox.log".format(LOG_DIR),
-                             firefox_binary=binary, executable_path='/tools/geckodriver/geckodriver')
+    request.addfinalizer(module_teardown)    
 
 
-@pytest.fixture(scope="module")
-def driver():
-    profile = new_profile("Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0")
-    driver = new_driver(profile)
-    driver.set_window_position(0, 0)
-    driver.set_window_size(1024, 768)
-    return driver
-    
-    
-@pytest.fixture(scope="module")
-def mobile_driver():    
-    profile = new_profile("Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16")
-    driver = new_driver(profile)
-    driver.set_window_position(0, 0)
-    driver.set_window_size(400, 2000)
-    return driver
-    
+def test_start(module_setup, app, domain, device_host):
+    add_host_alias_by_ip(app, domain, device_host)
 
-def test_index(driver, mobile_driver, app_domain):
+
+def test_index(driver, app_domain, ui_mode, screenshot_dir):
     url = "https://{0}".format(app_domain)
     driver.get(url)
     mobile_driver.get(url)
     time.sleep(10)
     
-    screenshots(driver, 'index')
-    screenshots(mobile_driver, 'index-mobile')
+    screenshots(driver, screenshot_dir, 'index-' + ui_mode)
+
     
-    
-def test_desktop_signup(driver, mobile_driver, app_domain):
+def test_signup(driver, app_domain, ui_mode, screenshot_dir):
     driver.get("https://{0}/users/sign_up".format(app_domain))
     time.sleep(10)
     
-    screenshots(driver, 'signup')
+    screenshots(driver, screenshot_dir, 'signup-' + ui_mode)
 
     user_email = driver.find_element_by_id("user_email")
     user_email.send_keys('user@example.com')
@@ -89,38 +63,20 @@ def test_desktop_signup(driver, mobile_driver, app_domain):
     user_password_confirmation.send_keys(Keys.RETURN)
     user_password_confirmation.submit()
 
-    screenshots(driver, 'signup-credentials')
+    screenshots(driver, screenshot_dir, 'signup-credentials-' + ui_mode)
 
     time.sleep(100)
     
-    screenshots(driver, 'signup-done')
+    screenshots(driver, screenshot_dir, 'signup-done-' + ui_mode)
 
 
-def test_mobile_signin(mobile_driver, app_domain):
-
-    mobile_driver.get("https://{0}/users/sign_in".format(app_domain))
-    screenshots(mobile_driver, 'signin-mobile')
-
-    user_username = mobile_driver.find_element_by_id("user_username")
-    user_username.send_keys('username')
-    user_password = mobile_driver.find_element_by_id("user_password")
-    user_password.send_keys('password')
-    user_password.submit()
-
-    screenshots(mobile_driver, 'signin-mobile-progress')
-
-    time.sleep(10)
-
-    screenshots(mobile_driver, 'signin-mobile-done')
-
-
-def test_stream(driver, app_domain):
+def test_stream(driver, app_domain, ui_mode, screenshot_dir):
 
     url = "https://{0}/stream".format(app_domain)
     driver.get(url)
     time.sleep(10)
     
-    screenshots(driver, 'stream-first-time')
+    screenshots(driver, screenshot_dir, 'stream-first-time-' + ui_mode)
     
     driver.find_element_by_css_selector('.popover-title .close').click()
     time.sleep(10)
@@ -129,19 +85,10 @@ def test_stream(driver, app_domain):
     driver.find_element_by_css_selector('.popover-title .close').click()
     time.sleep(10)
     
-    screenshots(driver, 'stream')
+    screenshots(driver, screenshot_dir, 'stream+' + ui_mode)
     
 
-def test_mobile_stream(mobile_driver, app_domain):
-
-    url = "https://{0}/stream".format(app_domain)
-    mobile_driver.get(url)
-    time.sleep(10)
-    
-    screenshots(mobile_driver, 'stream-first-time-mobile')
-    
-
-def test_post(driver, app_domain):
+def test_post(driver, app_domain, ui_mode, screenshot_dir):
    
     url = "https://{0}/stream".format(app_domain)
     driver.get(url)
@@ -152,60 +99,29 @@ def test_post(driver, app_domain):
     status_message_text.submit()
     time.sleep(10)
     
-    screenshots(driver, 'post')
+    screenshots(driver, screenshot_dir, 'post-' + ui_mode)
  
 
-def test_mobile_post(mobile_driver, app_domain):
-   
-    url = "https://{0}/stream".format(app_domain)
-    mobile_driver.get(url)
-    
-    time.sleep(10)
-    screenshots(mobile_driver, 'post-mobile')
-
-
-def test_profile_picture(driver, mobile_driver, app_domain):
+def test_profile_picture(driver, app_domain, ui_mode, screenshot_dir):
    
     url = "https://{0}/profile/edit".format(app_domain)
     driver.get(url)
-    mobile_driver.get(url)    
     time.sleep(10)
         
-    screenshots(driver, 'profile')
-    screenshots(mobile_driver, 'profile-mobile')
+    screenshots(driver, screenshot_dir, 'profile-' + ui_mode)
     
-    file = mobile_driver.find_element_by_css_selector('input[type="file"]')
-    mobile_driver.execute_script("arguments[0].removeAttribute('style')", file)
+    file = driver.find_element_by_css_selector('input[type="file"]')
+    driver.execute_script("arguments[0].removeAttribute('style')", file)
     time.sleep(2)
-    screenshots(mobile_driver, 'profile-mobile-file-style')
+    screenshots(driver, screensho_dir, 'profile-file-style-' + ui_mode)
     
     file.send_keys(join(DIR, 'images', 'profile.jpeg'))
-    screenshots(mobile_driver, 'profile-mobile-before-new-picture')
+    screenshots(driver, screenshot_dir, 'profile-before-new-picture-' + ui_mode)
     
     #file.submit()
 
     time.sleep(10)
 
-    screenshots(mobile_driver, 'profile-mobile-new-picture')
-    
-    driver.get(url)    
-    time.sleep(10)
-    screenshots(driver, 'profile-new-picture')
+    screenshots(driver, screenshot_dir, 'profile-new-picture-' + ui_mode)
     
 
-def screenshots(driver, name):
- 
-    driver.get_screenshot_as_file(join(screenshot_dir, '{0}.png'.format(name)))
-  
-    with open(join(screenshot_dir, '{0}.html.log'.format(name)), "w") as f:
-        f.write(driver.page_source.encode("utf-8"))
-   
-    with open(join(screenshot_dir, '{0}.js.log'.format(name)), "w") as f:
-        try:
-            f.write(str(driver.execute_script('return window.JSErrorCollector_errors ? window.JSErrorCollector_errors.pump() : []')))
-        except WebDriverException, e:
-            print("unable to get js errors: {0}".format(e))
-
-
-
- 
